3ec62a4f23c51fa1146c4fa96e2807acacd0f568
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2020-11-02 20:56:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+var form,player,game;
+var playerCount,gameState = 0;
+
+
+function setup(){
+
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  
+}
+
